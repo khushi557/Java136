a7099894b9f8dd05b566f3e925c7bd27b88ace8f
khushi557 <kathakkartik@gmail.com>
2025-09-01 15:31:51 +0530

    2d array

---
+++ src/Array3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int a[][]=new int [2][3];
+        int b[][]=new int [2][3];
+        int c[][]=new int [2][3];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<2;i++){
+            for( int j=0;j<3;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<2;i++){
+            for( int j=0;j<3;j++){
+                b[i][j]=sc.nextInt();
+            }
+        }
+
+
+        for( int i=0;i<2;i++){
+            for( int j=0;j<3;j++){
+                c[i][j]=a[i][j]+b[i][j];
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
